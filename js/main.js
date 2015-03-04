50259a567cd38311107b33cb0a3dd036ac0257b6
Dmitry Myadzelets <DmitryMyadzelets@gmail.com>
2015-03-04 15:24:10 +0100

    Fixed some bugs impoved local saving

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // JSLint options:
-/*global jA*/
+/*global d3, jas, ResizeSensor, Blob, saveAs*/
 
-(function app () {
+(function app() {
 
     var container = document.getElementById('svg_container');
-    var editor = jA.editor.instance(container);
+    var editor = new jas.editor.Instance(container);
 
 
     function resize() {
@@ -16,7 +16,7 @@
     new ResizeSensor(container, resize);
 
 
-    function load_graph (name) {
+    function load_graph(name) {
         if ($ && $.jStorage && $.jStorage.storageAvailable()) {
             // $.jStorage.flush();
             return JSON.parse($.jStorage.get(name));
@@ -25,7 +25,7 @@
     }
 
 
-    function save_graph (graph, name) {
+    function save_graph(graph, name) {
         if ($ && $.jStorage && $.jStorage.storageAvailable()) {
             var s = JSON.stringify(graph);
             // console.log(s);
@@ -33,29 +33,68 @@
         }
     }
 
-    jA.editor.commands.on['update'] = function () {
-        var graph = editor.graph.storable();
-        save_graph(graph, 'graph');
-    };
+    /**
+     * State machine which calls save method after the last change when its timer expires.
+     * It accepts two events: 
+     * 1. 'update' from an editor.
+     * 2. Timer event (the timer is set by the machine itslef).
+     */
+    var save_controller = (function () {
+        "use strict";
+        var timer, counter;
+
+        function tout() { state(); }
+
+        var state, states = {
+            init : function () {
+                counter = 0;
+                timer = setInterval(tout, 500);
+                state = states.wait_for_tout;
+            },
+            wait_for_tout : function (event) {
+                if (event === 'update') {
+                    counter = 0;
+                } else {
+                    // Wait for at least 2 consiquent timer events
+                    if (++counter > 1) {
+                        clearInterval(timer);
+                        save_graph(editor.graph.get_json(), 'graph');
+                        state = states.init;
+                    }
+                }
+            }
+        };
+        state = states.init;
+
+        return function loop() {
+            state.apply(this, arguments);
+            return loop;
+        };
+    }());
 
 
-    (function init () {
+    jas.after(editor.commands, 'update', function () {
+        save_controller('update');
+    });
+
+
+    (function init() {
         var graph = load_graph('graph');
         if (graph && graph.nodes) {
-            editor.set_graph(graph);
+            editor.graph.set_json(graph);
         } else {
             var x = container.offsetWidth / 4;
             var y = container.offsetHeight / 3;
-            editor.set_graph({
+            editor.graph.set_json({
                 nodes : [
-                    { x : x, y : y, initial : true, text : 'off' }, 
+                    { x : x, y : y, initial : true, text : 'off' },
                     { x : x + 150, y : y, text : 'on' },
                     { x : x + 150, y : y + 100, marked : true}
                 ],
                 edges : [
                     {source : 0, target : 1, text : 'up'},
                     {source : 1, target : 1, text : '*'},
-                    {source : 1, target : 0, text : 'down'}, 
+                    {source : 1, target : 0, text : 'down'},
                     {source : 1, target : 2, text : 'hello world'}
                 ]
             });
@@ -77,7 +116,7 @@
         // Make a fake parent node in oder do delete copy of SVG late
         var foo = document.createElement('foo');
         foo.appendChild(svg);
-        
+
         var defs = foo.getElementsByTagName('defs')[0];
         if (defs) {
             var style = defs.getElementsByTagName('style')[0];
@@ -105,12 +144,12 @@
 
 
     d3.select('#btn_undo').on('click', function () {
-        alert('undo');
+        editor.commands.undo();
     });
 
 
     d3.select('#btn_redo').on('click', function () {
-        alert('redo');
+        editor.commands.redo();
     });
 
 }());
